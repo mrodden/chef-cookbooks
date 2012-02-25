3259d701c702f1e4a0b03de775e302f88a5c7c33
locke105 <locke105@gmail.com>
2012-02-25 00:35:31 -0600

    updated maintainer information

--- libpam-krb5/metadata.rb
+++ libpam-krb5/metadata.rb
@@ -1,5 +1,5 @@
-maintainer       "Matt Odden"
-maintainer_email "mathew.odden@ndsu.edu"
+maintainer       "Mathew Odden"
+maintainer_email "locke105@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures libpam-krb5 on ubuntu/debian systems"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
