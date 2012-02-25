71670f984c06b6594c539636e2163ea8ec7aab2b
locke105 <locke105@gmail.com>
2012-02-25 00:33:56 -0600

    updated maintainer information

--- ndsu-openstack/metadata.rb
+++ ndsu-openstack/metadata.rb
@@ -1,7 +1,7 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
-description      "Installs/Configures ndsu-openstack"
+maintainer       "Mathew Odden"
+maintainer_email "locke105@gmail.com"
+license          "Apache 2.0"
+description      "Installs/Configures ndsu-openstack nodes"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 depends		 "libpam-krb5"
